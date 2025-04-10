787b8e29ca767ea3eacb7741e7f32806754109b8
Leninesouza <leninesouza64@gmail.com>
2025-04-10 15:19:39 -0300

    mudança na aplicacao porque ainda nao tem banco

--- src/main/java/br/com/llfw/SpringECommerce/SpringECommerceApplication.java
+++ src/main/java/br/com/llfw/SpringECommerce/SpringECommerceApplication.java
@@ -2,8 +2,10 @@ package br.com.llfw.SpringECommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// Anotação pra quando ainda não tem banco
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringECommerceApplication {
 
 	public static void main(String[] args) {
