2747c6bf66d06f4938c98d7f295fce24f1a95438
Leninesouza <leninesouza64@gmail.com>
2025-04-10 14:36:55 -0300

    first commit

---
+++ src/main/java/br/com/llfw/SpringECommerce/SpringECommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.llfw.SpringECommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/llfw/SpringECommerce/SpringECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.llfw.SpringECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
